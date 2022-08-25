12ff8a313d2f347fa70ce2414a26061b01ea9a58
thairyo <sonthai1310.works@gmail.com>
2022-08-25 21:03:14 +0700

    create popup.

--- src/MapBox.js
+++ src/MapBox.js
@@ -1,6 +1,5 @@
-import axios from "axios";
 import React, { useState } from "react";
-import { Map, Marker, Popup } from "react-map-gl";
+import Map, { Marker, Popup } from "react-map-gl";
 
 export default function MapBox() {
   const [viewport, setViewport] = useState({
@@ -9,22 +8,19 @@ export default function MapBox() {
     zoom: 16,
   });
 
+
   const [showPopup, setShowPopup] = useState(true);
 
   const handleTogglePopup = () => {
-    setShowPopup(!showPopup);
-  };
-
-  const handleMoveMap = (viewport) => {
-    setViewport(viewport);
-  };
+    setShowPopup(!showPopup)
+  }
 
   return (
     <Map
       {...viewport}
       style={{ width: "100vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
-      onMove={(e) => handleMoveMap()}
+      onMove={(e) => setViewport(e.viewport)}
       mapboxAccessToken="pk.eyJ1IjoidGhhaXJ5byIsImEiOiJjbDc4YWI5b3owN3U2M29zOXVvYnB3amw4In0.XEiN9BrQ_IMtm9c3TAcJYQ"
     >
       <Marker
@@ -33,21 +29,22 @@ export default function MapBox() {
         anchor="bottom"
       >
         <img
-          style={{ height: 50, width: 50 }}
+          style={{ height: 50, width: 50, cursor: "pointer" }}
           onClick={handleTogglePopup}
           src="https://xuonginthanhpho.com/wp-content/uploads/2020/03/map-marker-icon.png"
           alt="marker"
         />
-        {showPopup && (
-          <Popup
-            longitude={108.209398875}
-            latitude={16.060034379}
-            anchor="top-right"
-          >
-            You are here
-          </Popup>
-        )}
       </Marker>
+      {showPopup && (
+        <Popup
+          latitude={16.060034379945296}
+          longitude={108.20939887509337}
+          anchor="top-right"
+          closeOnClick={false}
+        >
+          You are here
+        </Popup>
+      )}
     </Map>
   );
 }
