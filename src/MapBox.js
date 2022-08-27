2b06c409858d7b4a1a2e0c5a214592f0f57e28eb
thairyo <sonthai1310.works@gmail.com>
2022-08-27 13:08:07 +0700

    init location.

--- src/MapBox.js
+++ src/MapBox.js
@@ -1,46 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Map, { Marker, Popup } from "react-map-gl";
-import axios from "axios";
+import { initialData } from "./actions/initialData";
 
 export default function MapBox() {
   const [viewport, setViewport] = useState({
-    latitude: 16.060034379945296,
-    longitude: 108.20939887509337,
+    latitude: 16.082620606761385,
+    longitude: 108.22371699783464,
     zoom: 16,
   });
 
-  const [showPopup, setShowPopup] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
   const handleTogglePopup = () => {
     setShowPopup(!showPopup);
   };
 
-  const [addressMarker, setAddressMarker] = useState([]);
-
-  const addressData = [
-    { id: 1, address: "Bến xe Đà Nẵng" },
-    { id: 2, address: "121 Tôn Đức Thắng, Đà Nẵng" },
-    { id: 3, address: "721 Điện Biên Phủ, Đà Nẵng" },
-  ];
-
-  useEffect(() => {
-    let newAddressData = [];
-    addressData.map((adr) => {
-      axios
-        .get(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${adr.address}.json?access_token=pk.eyJ1IjoidGhhaXJ5byIsImEiOiJjbDc4OTMzNzkwN2ZzM3ZueXE0NWdyNHB0In0.G_TZ_zbzQ8T7512A44nK9g`
-        )
-        .then((res) => {
-          newAddressData.push({
-            ...addressData,
-            longitude: res.data.features[0].center[0],
-            latitude: res.data.features[0].center[1],
-          });
-        })
-        .catch((error) => console.log(error));
-    });
-    console.log(">>>newData", newAddressData);
-    setAddressMarker(newAddressData);
-  }, []);
 
   return (
     <Map
@@ -50,30 +23,32 @@ export default function MapBox() {
       onMove={(e) => setViewport(e.viewport)}
       mapboxAccessToken="pk.eyJ1IjoidGhhaXJ5byIsImEiOiJjbDc4YWI5b3owN3U2M29zOXVvYnB3amw4In0.XEiN9BrQ_IMtm9c3TAcJYQ"
     >
-      {addressMarker.map((addressm) => (
-        <Marker
-          key={addressm.id}
-          latitude={addressm.latitude}
-          longitude={addressm.longitude}
-          anchor="bottom"
-        >
-          <img
-            style={{ height: 50, width: 50, cursor: "pointer" }}
-            onClick={handleTogglePopup}
-            src="https://xuonginthanhpho.com/wp-content/uploads/2020/03/map-marker-icon.png"
-            alt="marker"
-          />
-        </Marker>
-      ))}
-      {showPopup && (
-        <Popup
-          latitude={16.060034379945296}
-          longitude={108.20939887509337}
-          anchor="top-right"
-          closeOnClick={false}
-        >
-          You are here
-        </Popup>
+      {initialData.busRoutes.map((addressm) =>
+        addressm.route.map((i) => (
+          <>
+            <Marker
+              key={i.id}
+              latitude={i.location.lat}
+              longitude={i.location.lng}
+              anchor="bottom"
+            >
+              <img
+                style={{ height: 50, width: 50, cursor: "pointer" }}
+                onClick={handleTogglePopup}
+                src="https://xuonginthanhpho.com/wp-content/uploads/2020/03/map-marker-icon.png"
+                alt="marker"
+              />
+            </Marker>
+            <Popup
+              latitude={i.location.lat}
+              longitude={i.location.lng}
+              anchor="top-right"
+              closeOnClick={true}
+            >
+              {i.name}
+            </Popup>
+          </>
+        ))
       )}
     </Map>
   );
---
+++ src/actions/initialData.js
@@ -0,0 +1,35 @@
+export const initialData = {
+  busRoutes: [
+    {
+      id: "bus-routes-1",
+      name: "R4A: Cảng Sông Hàn U+2013 Hòa Tiến​",
+      title: "Vé lượt: 6.000 đồng/lượt.",
+      route: [
+        {
+          id: "1",
+          name: "Cảng Sông Hàn",
+          location: {
+            lng: 108.22371699783464,
+            lat: 16.082620606761385,
+          },
+        },
+        {
+          id: "2",
+          name: "20 Đống Đa",
+          location: {
+            lng: 108.22133029968681,
+            lat: 16.082657169371853,
+          },
+        },
+        {
+          id: "3",
+          name: "104-106 Đống Đa",
+          location: {
+            lng: 108.21938774519234,
+            lat: 16.081034242395134,
+          },
+        },
+      ],
+    },
+  ],
+};
