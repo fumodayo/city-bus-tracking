2d32e860acad9337c2c4e0e636e8f4b2b940a697
thairyo <sonthai1310.works@gmail.com>
2022-10-08 14:32:44 +0700

    updated data locationData & travelData & informationBusRouteData.

--- src/actions/initialData/locationData.js
+++ src/actions/initialData/locationData.js
@@ -9,14 +9,6 @@ export const locationData = [
      Cầu Rồng - Ngô Quyền - Ngũ Hành Sơn - Lê Văn Hiến - Trần Đại Nghĩa - Đại học Việt Hàn (Nam Kỳ Khởi Nghĩa).`,
     lineDistance: '31,63km',
     operatingTime: 'Từ 06h00 đến 21h00 (15 phút/chuyến)',
-    price: [
-      'Vé lượt: 6.000 đồng/lượt',
-      'Vé tháng ưu tiên 65.000 đồng/tháng.',
-      'Vé tháng không ưu tiên 130.000 đồng/tháng.'
-    ],
-    linkMonthlyTicket: 'https://www.danangbus.vn/thu-tuc-lam-ve-thang.html',
-    linkOnline:
-      'https://www.danangbus.vn/tin-tuc/tin-tuc/huong-dan-dang-ky-the-ve-thang-truc-tuyen-1135.html',
     route: [
       {
         id: 41676731701,
@@ -528,8 +520,13 @@ export const locationData = [
     id: 'bus-routes-2',
     nameBusRouter: 'R16',
     name: 'R16: Kim Liên-Đại học Việt Hàn',
-    title: 'Vé lượt: 6.000 đồng/lượt.',
     directionRoute: 'return',
+    drivingJourney: `Đại học Việt Hàn (Nam Kỳ Khởi Nghĩa) - Trần Đại Nghĩa - Lê Văn Hiến -
+     Ngũ Hành Sơn - Ngô Quyền - Cầu Rồng - Bạch Đằng - Hùng Vương - Lý Thái Tổ - Điện Biên Phủ - 
+    Cầu vượt ngã 3 Huế (tầng 3) - Tôn Đức Thắng - Nguyễn Lương Bằng - Cầu Nam Ô - Nguyễn Văn Cừ - 
+    Kim Liên`,
+    lineDistance: '31,53km',
+    operatingTime: 'Từ 06h00 đến 21h00 (15 phút/chuyến)',
     route: [
       {
         id: 61728855973,
@@ -1052,6 +1049,12 @@ export const locationData = [
     nameBusRouter: 'R4A',
     name: 'R4A: Cảng Sông Hàn-Hòa Tiến',
     directionRoute: 'turn',
+    drivingJourney: `<strong>Cảng Sông Hàn (bãi đỗ xe đầu đường Như Nguyệt) </strong>- 
+    Đống Đa - Ông Ích Khiêm - Nguyễn Văn Linh - Đường 2/9 - Tiểu La - Núi Thành - Đường 30/4 -
+    Nguyễn Hữu Thọ - Lê Đại Hành - Ông Ích Đường - CMT8 - Trường Chinh - Cầu Đỏ - QL1A - ĐT605 - 
+    Chợ Lệ Trạch -<strong>Hòa Tiến (Khu đất đối diện Nghĩa trang xã Hòa Tiến)</strong>`,
+    lineDistance: '20,00 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30p/chuyến)',
     route: [
       {
         id: 16738996829,
@@ -1348,6 +1351,13 @@ export const locationData = [
     nameBusRouter: 'R4A',
     name: 'R4A: Cảng Sông Hàn-Hòa Tiến',
     directionRoute: 'return',
+    drivingJourney: `<strong>Hòa Tiến (DT605, đối diện Nghĩa trang xã Hòa Tiến) </strong>-
+     Chợ Lệ Trạch - ĐT605 - QL1A - Cầu Đỏ - Trường Chinh - CMT8 - Ông Ích Đường - Lê Đại Hành -
+      Nguyễn Hữu Thọ - Đường 30/4 - Núi  Thành - Tiểu  La - 
+    Đường 2/9 - Nguyễn Văn Linh - Ông Ích Khiêm - Đống Đa - 
+    <strong>Cảng Sông Hàn (bãi đỗ xe đầu đường Như Nguyệt.</strong>`,
+    lineDistance: '20,00 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30p/chuyến)',
     route: [
       {
         id: 39502269135,
@@ -1653,6 +1663,11 @@ export const locationData = [
     nameBusRouter: 'R17A',
     name: 'R17A: Cảng Sông Hàn-TTHC huyện Hòa Vang',
     directionRoute: 'turn',
+    drivingJourney: `<strong>Cảng Sông Hàn (bãi đỗ xe đầu đường Như Nguyệt) </strong>- Trần Phú - Lý Tự Trọng -
+     Nguyễn Thị Minh Khai - Hùng Vương - Trần Phú - Trưng Nữ Vương - Núi Thành - Cách Mạng Tháng 8 - 
+     Cầu Vượt Hòa Cầm - Trường Sơn - Quốc lộ 14B - Quảng Xương - <strong> TTHC Huyện Hòa Vang.</strong>`,
+    lineDistance: '20,00 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (15 phút/chuyến)',
     route: [
       {
         id: 4677255836,
@@ -1877,6 +1892,11 @@ export const locationData = [
     nameBusRouter: 'R17A',
     name: 'R17A: Cảng Sông Hàn-TTHC huyện Hòa Vang',
     directionRoute: 'return',
+    drivingJourney: `<strong>TTHC Huyện Hòa Vang</strong> - Quảng Xương - Quốc lộ 14B - Trường Sơn - 
+    Cầu Vượt Hòa Cầm - CMT8 - Núi Thành - Phan Thành Tài- Đường 2/9 - Bạch Đằng - Hùng Vương - 
+    Nguyễn Thị Minh Khai - Lý Tự Trọng - Bạch Đằng - <strong>Cảng Sông Hàn (bãi đỗ xe đầu đường Như Nguyệt).</strong>`,
+    lineDistance: '20,20 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (15 phút/chuyến)',
     route: [
       {
         id: 55793403782,
@@ -2101,6 +2121,12 @@ export const locationData = [
     nameBusRouter: 'R6A',
     name: 'R6A: Bến xe Trung tâm-Khu du lịch Non Nước',
     directionRoute: 'turn',
+    drivingJourney: `<strong>Bến xe Trung Tâm Đà Nẵng</strong> - Cao Sơn Pháo - Lê Thạch - Tú Mỡ - Đinh Liệt - 
+    Nguyễn Đình Tứ - Tôn Đản - Lê Trọng Tấn - Trường Chinh - Lê Đại Hành - Nguyễn Hữu Thọ - 
+    Đường 30 Tháng 4 - Núi Thành - Phan Đăng Lưu - Đường 2 Tháng 9 - Cầu Tiên Sơn - Ngũ Hành Sơn - 
+    Phan Tứ - Võ Nguyên Giáp - Trường Sa - Non Nước - <strong>KDL Non Nước.</strong>`,
+    lineDistance: '21,80 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 74185081170,
@@ -2361,6 +2387,12 @@ export const locationData = [
     nameBusRouter: 'R6A',
     name: 'R6A: Bến xe Trung tâm-Khu du lịch Non Nước',
     directionRoute: 'return',
+    drivingJourney: `<strong>KDL Non Nước</strong> - Non Nước - Trường Sa - Võ Nguyên Giáp - Phan Tứ - 
+    Ngũ Hành Sơn - Cầu Tiên Sơn - Đường 2 Tháng 9 - Phan Đăng Lưu - Núi Thành - Đường 30 Tháng 4 - 
+    Nguyễn Hữu Thọ - Lê Đại Hành - Trường Chinh - Lê Trọng Tấn - Tôn Đản - Nguyễn Đình Tứ - Đinh Liệt - 
+    Tú Mỡ - Nguyễn Văn Tạo - Cao Sơn Pháo - <strong>Bến Xe Trung Tâm Đà Nẵng.</strong>`,
+    lineDistance: '21,40 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 45798943743,
@@ -2630,6 +2662,12 @@ export const locationData = [
     nameBusRouter: 'R15',
     name: 'R15: Bến xe Trung tâm-Thọ Quang',
     directionRoute: 'turn',
+    drivingJourney: `<strong>Bến xe Trung tâm Đà Nẵng</strong> - Tôn Đức Thắng - Cầu vượt ngã Ba Huế (tầng 3) - 
+    Điện Biên Phủ - Huỳnh Ngọc Huệ - Hà Huy Tập - Thái Thị Bôi - Lê Độ - Võ Văn Tần - Hải Phòng - 
+    Ông Ích Khiêm - Nguyễn Hoàng - Nguyễn Văn Linh -  Cầu Rồng - Võ Văn Kiệt - Hồ Nghinh - Hà Kỳ Ngộ -
+     Nguyễn Sáng - Nguyễn Huy Chương - Hoàng Sa - <strong>Thọ Quang</strong>`,
+    lineDistance: '16,16 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 92537656386,
@@ -2881,6 +2919,12 @@ export const locationData = [
     nameBusRouter: 'R15',
     name: 'R15: Bến xe Trung tâm-Thọ Quang',
     directionRoute: 'return',
+    drivingJourney: `<strong>Thọ Quang</strong> - Hoàng Sa - Nguyễn Huy Chương - Nguyễn Sáng - Hà Kỳ Ngộ - 
+    Hồ Nghinh - Võ Văn Kiệt - Cầu Rồng - Nguyễn Văn Linh - Nguyễn Hoàng - Ông Ích Khiêm - 
+    Hải Phòng - Võ Văn Tần - Lê Độ - Thái Thị Bôi - Hà Huy Tập - Huỳnh Ngọc Huệ - Điện Biên Phủ - 
+    Cầu vượt ngã Ba Huế (tầng 3) - Tôn Đức Thắng - <strong>Bến xe Trung tâm Đà Nẵng</strong>`,
+    lineDistance: '16,16 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 12302050934,
@@ -3123,6 +3167,13 @@ export const locationData = [
     nameBusRouter: 'TMF',
     name: 'TMF: Vũng Thùng-Công Viên 29/3-CV Biển Đông',
     directionRoute: 'turn',
+    drivingJourney: `<strong>BX Bùi Dương Lịch </strong>- Bùi Dương Lịch - Dương Vân Nga - Khúc Hạo - Hồ Hán Thương - 
+    Chu Huy Mân - Cầu Thuận Phước - Đường 3/2 - Xuân Diệu - Trạm xe buýt Xuân Diệu - Xuân Diệu - 
+    Đường 3/2 - Trần Phú - Lý Tự Trọng - Nguyễn Thị Minh Khai - Hải Phòng - Ngô Gia Tự - Hùng Vương - 
+    Lý Thái Tổ - Điện Biên Phủ - Nguyễn Tri Phương - Nguyễn Văn Linh - Cầu Rồng - Võ Văn Kiệt - 
+    Võ Nguyên Giáp - <strong>CV Biển Đông</strong>`,
+    lineDistance: '16,1 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 54081045721,
@@ -3392,6 +3443,13 @@ export const locationData = [
     nameBusRouter: 'TMF',
     name: 'TMF: Vũng Thùng-Công Viên 29/3-CV Biển Đông',
     directionRoute: 'return',
+    drivingJourney: `<strong>CV Biển Đông</strong> - Võ Nguyên Giáp - Võ Văn Kiệt - Cầu Rồng - Nguyễn Văn Linh - 
+    Nguyễn Tri Phương - Điện Biên Phủ - Lý Thái Tổ - Hùng Vương - Ngô Gia Tự - Hải Phòng -
+     Nguyễn Thị Minh Khai - Lý Tự Trọng - Bạch Đằng - Đường 3/2  - Xuân Diệu - Trạm xe buýt Xuân Diệu - 
+     Xuân Diệu - Đường 3/2 -  Cầu Thuận Phước - Chu Huy Mân - Hồ Hán Thương - Khúc Hạo - Dương Vân Nga 
+     - Bùi Dương Lịch - <strong>BX Bùi Dương Lịch</strong>`,
+    lineDistance: '16,5 km',
+    operatingTime: 'Từ 06h00 đến 19h00 (30 phút/chuyến)',
     route: [
       {
         id: 45910068255,
