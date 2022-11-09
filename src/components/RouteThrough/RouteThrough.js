7f5d2d8972f98f2598843b777e2b2e05dec1ff1d
thairyo <sonthai1310.works@gmail.com>
2022-11-09 14:12:46 +0700

    open sidebar in bus through.

--- src/components/RouteThrough/RouteThrough.js
+++ src/components/RouteThrough/RouteThrough.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { routesData } from 'actions/initialData/routesData'
 import { busStopData } from 'actions/initialData/busStopData'
+import ListBusStop from 'components/ListBusStop/ListBusStop'
+import CustomSidebar from 'components/Common/CustomSidebar'
 
 const RouteThrough = ({ idBusStop }) => {
   const [routeThrough, setRouteThrough] = useState([])
@@ -19,35 +21,64 @@ const RouteThrough = ({ idBusStop }) => {
     setRouteThrough(routebusthroungh)
   }, [idBusStop])
 
+  const [openSidebar, setOpenSidebar] = useState(false)
+  const handleOpenSidebarBusList = () => {
+    setOpenSidebar(openSidebar => !openSidebar)
+  }
   return (
-    <div className="route-through">
-      <div className="scroll-content">
-        <div
-          style={{ cursor: 'pointer' }}
-          className="row align-items-center h-100"
-        >
-          <div className="small-3">
-            <div
-              className="route-no text-center"
-              style={{ background: `${routeThrough.colorRoute}` }}
-            >
-              <span>{routeThrough.codeBusRoute}</span>
+    <>
+      <div className="route-through">
+        <div className="scroll-content">
+          <div
+            style={{ cursor: 'pointer' }}
+            className="row align-items-center h-100"
+            onClick={handleOpenSidebarBusList}
+          >
+            <div className="small-3">
+              <div
+                className="route-no text-center"
+                style={{ background: `${routeThrough.colorRoute}` }}
+              >
+                <span>{routeThrough.codeBusRoute}</span>
+              </div>
             </div>
-          </div>
 
-          <div className="small-7">
-            <p className="code-route">{routeThrough.nameRoute}</p>
-            <p
-              className="code-desc"
-              style={{ fontSize: '14.5px', color: '#1F8BAE' }}
-            >
-              {routeThrough.directionRoute === 'turn' ? 'Chiều đi' : 'Chiều về'}
-            </p>
+            <div className="small-7">
+              <p className="code-route">{routeThrough.nameRoute}</p>
+              <p
+                className="code-desc"
+                style={{ fontSize: '14.5px', color: '#1F8BAE' }}
+              >
+                {routeThrough.directionRoute === 'turn'
+                  ? 'Chiều đi'
+                  : 'Chiều về'}
+              </p>
+            </div>
+            <hr></hr>
           </div>
-          <hr></hr>
         </div>
       </div>
-    </div>
+      {openSidebar && (
+        <CustomSidebar
+          show={true}
+          name={routeThrough.nameRoute}
+          tabLeft={'Xem lượt đi'}
+          tabRight={'Xem lượt về'}
+          compLeft={
+            <ListBusStop
+              nameCodeRoute={routeThrough.codeBusRoute}
+              turnRoute={'turn'}
+            />
+          }
+          compRight={
+            <ListBusStop
+              nameCodeRoute={routeThrough.codeBusRoute}
+              turnRoute={'return'}
+            />
+          }
+        />
+      )}
+    </>
   )
 }
 
